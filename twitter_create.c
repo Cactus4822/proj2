7ef754dd0902dafe35d1f05a3ee800a575415687
Ehkso <austn.secnd@gmail.com>
2022-05-03 17:14:00 +0100

    Added start of code to twitter_create.c just to show that I'm workin on it

--- twitter_create.c
+++ twitter_create.c
@@ -8,6 +8,23 @@
 
 void create_twitter_system(twitter * twitter_system){
 
-    //to be completed
+    int num_users;
+
+    printf("Enter number of users: \n");
+    scanf("%d", &num_users);
+    while (num_users < 1){
+        printf("Error: Need at least 1 user.\n");
+        printf("Enter number of users: \n");
+        scanf("%d", &num_users);
+    }
+    printf("%d users.\n", num_users);
+
+    user users[num_users];
+    tweet news_feed[MAX_TWEETS];
+
+
+    for (int i=0; i<num_users;i++){
+        printf("Username of next user (Press Enter to end): ");
+    }
 
 }
