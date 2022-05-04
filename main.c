120553be77ac6acbf98e0538a9baaaf0e43e6a7c
Ehkso <austn.secnd@gmail.com>
2022-05-04 20:21:39 +0100

    Not done

--- main.c
+++ main.c
@@ -8,9 +8,20 @@ int main() {
     create_twitter_system(&twitter_system);
     printf("\n%d users in t_s.n_u\n", twitter_system.num_users);
 
-    for(int i =0; i < twitter_system.num_users; i++){
-        user usr = twitter_system.users[i];
+    int turn=0, i;
+    user usr = twitter_system.users[i];
+
+    for(i=0; i < twitter_system.num_users; i++){
         printf("User: %s; Followers: %d; Following: %d\n",usr.username, usr.num_followers, usr.num_followers);
+    }
+
+    i=0;
+    while (turn != -1){
+        if (i < twitter_system.num_users){
+            i = 0;
+        }
+        printf("%s is in control. Select an operation:\n", usr.username);
+
     }
 
 
