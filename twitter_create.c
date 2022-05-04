47b4e17c3d718deec90fc012045d95ac72f4e9f5
Ehkso <austn.secnd@gmail.com>
2022-05-04 19:31:13 +0100

    users became twitter_system->users. also brought back changing \n to \0 but it doesn't seem to have worked.

--- twitter_create.c
+++ twitter_create.c
@@ -19,29 +19,52 @@ void create_twitter_system(twitter * twitter_system){
     int num_users; // Number of users ygm
 
     printf("Enter number of users: \n"); // Gets number of users
-    scanf("%d", &num_users);
+    scanf("%d", &twitter_system->num_users);
     getchar();
-    twitter_system->num_users = num_users;
-    while (num_users < 1){ // Makes sure there's at least one user
+    //twitter_system->num_users = num_users;
+    while (twitter_system->num_users < 1){ // Makes sure there's at least one user
         printf("Error: Need at least 1 user.\n");
         printf("Enter number of users: \n");
-        scanf("%d", &num_users);
+        scanf("%d", &twitter_system->num_users);
     }
-    printf("%d users.\n", num_users); // Just to double-check
+    printf("%d users.\n", twitter_system->num_users); // Just to double-check
 
-    user users[num_users]; // Users Array
+    //user users[twitter_system->num_users]; // Users Array
+    twitter_system->users[twitter_system->num_users];
     tweet news_feed[MAX_TWEETS];
 
     printf("Username of first user:");
+    /*
     fgets(users[0].username, USR_LENGTH, stdin);
+    if (users[i].username[strlen(users[i].username) -1] == "\n")
+     users[i].username[strlen(users[i].username) -1] = "\0";
     users[0].num_followers = 0;
     users[0].num_following = 0;
+     */
+    fgets(twitter_system->users[0].username, USR_LENGTH, stdin);
+    if (twitter_system->users[0].username[strlen(twitter_system->users[0].username) -1] == "\n")
+        twitter_system->users[0].username[strlen(twitter_system->users[0].username) -1] = "\0";
+    twitter_system->users[0].num_followers = 0;
+    twitter_system->users[0].num_following = 0;
 
-    for (int i=1;i<num_users;i++){
+    for (int i=1;i<twitter_system->num_users;i++){
         printf("\nUsername of next user:");
+        /*
         fgets(users[i].username, USR_LENGTH, stdin);
+        if (users[i].username[strlen(users[i].username) -1] == "\n")
+         users[i].username[strlen(users[i].username) -1] = "\0";
         users[i].num_followers = 0;
         users[i].num_following = 0;
+         */
+        fgets(twitter_system->users[i].username, USR_LENGTH, stdin);
+        if (twitter_system->users[i].username[strlen(twitter_system->users[i].username) -1] == "\n")
+            twitter_system->users[i].username[strlen(twitter_system->users[i].username) -1] = "\0";
+        twitter_system->users[i].num_followers = 0;
+        twitter_system->users[i].num_following = 0;
+    }
+
+    for (int i=0;i<twitter_system->num_users;i++){
+        printf("%s ", twitter_system->users[i]);
     }
 
 }
